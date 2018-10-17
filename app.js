fa2ea52c7443d68b05d90ee1eecde31b4c2df7a3
Brittney <brittfort34@gmail.com>
2018-10-17 09:01:37 +0000

    reading user data from Express routes

--- app.js
+++ app.js
@@ -7,12 +7,15 @@
 var express = require('express');
 var app = express();
 
-var logger = require('./logger');
-app.use(logger);
+var blocks = {
+    'Fixed': 'Fastened securely in position', 
+    'Movable': 'Capable of being moved', 
+    'Rotating': 'Moving in a circle around its center'
+};
 
-/*app.get('/', function(req, res) {
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
-});*/
+});
 
 // add middleware to the application stack
 app.use(express.static('public'));
@@ -20,9 +23,36 @@ app.use(express.static('public'));
 
 // ?? Why is there no folder called blocks?
 app.get('/blocks', function(req, res) {
-   var blocks = ['Fixed', 'Movable', 'Rotating'];
-   res.json(blocks); 
+ 
+   
+   if (req.query.limit >= 0) {
+       res.json(blocks.slice(0, req.query.limit));
+   } else {
+       res.json(blocks); 
+   }
 });
+
+// create a dynamic route
+app.get('/blocks/:name', function(req, res) {
+    var description = blocks[req.params.name];
+    // handle error if no property is found for a given Block name
+    // check for presence of a description
+    if (!description) {
+        // display the status and a message
+        res.status(404).json('No description found for ' + req.params.name);
+    } else {
+         res.json(description);
+    }
+   
+});
+
+
+
+
+
+
+
+
 
 
 app.listen(8080, function() {
